a8536944e93ed3095b40673b0d8538fef5514837
96hoshi <m.locascio96@hotmail.com>
2020-08-12 18:30:41 +0200

    Added database

--- src/WQServer.java
+++ src/WQServer.java
@@ -17,24 +17,26 @@ public class WQServer {
 
 	public static void main(String[] args) {
 
-        int nThreads = 4;
-        long keepAliveTime = 1;
-        LinkedBlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>();
-		ThreadPoolExecutor tPool = new ThreadPoolExecutor(nThreads, nThreads + 2, keepAliveTime, TimeUnit.SECONDS, workQueue);
+		int nThreads = 4;
+		long keepAliveTime = 1;
+		LinkedBlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>();
+		ThreadPoolExecutor tPool = new ThreadPoolExecutor(nThreads, nThreads + 2, keepAliveTime, TimeUnit.SECONDS,
+				workQueue);
 		
+		WQDatabase db = new WQDatabase();
+
 		// Setting RMI method for registration
 		try {
-			RegistrationTask registrationService = new RegistrationTask(/*db*/);
+			RegistrationTask regService = new RegistrationTask(db);
 			// Export the object
-			RegistrationRemote stub = (RegistrationRemote) UnicastRemoteObject.exportObject(registrationService, PORT+1);
+			RegistrationRemote stub = (RegistrationRemote) UnicastRemoteObject.exportObject(regService, PORT + 1);
 			// creation of a registry
-			Registry registry = LocateRegistry.createRegistry(PORT+1);
+			Registry registry = LocateRegistry.createRegistry(PORT + 1);
 			registry.rebind("WQ-Registration", stub);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		System.out.println("Listening for connections on port " + PORT);
 		ServerSocketChannel serverChannel;
 		Selector selector;
@@ -43,7 +45,7 @@ public class WQServer {
 			serverChannel = ServerSocketChannel.open();
 			ServerSocket serverSock = serverChannel.socket();
 			InetSocketAddress address = new InetSocketAddress(PORT);
-			
+
 			serverSock.bind(address);
 			serverChannel.configureBlocking(false);
 			selector = Selector.open();
@@ -52,7 +54,7 @@ public class WQServer {
 			ex.printStackTrace();
 			return;
 		}
-		
+
 		while (true) {
 			try {
 				selector.select();
@@ -89,11 +91,9 @@ public class WQServer {
 							MessageWorker m = new MessageWorker();
 							Message input = m.readMessage(buffer);
 							handleClient(input, client, selector, tPool);
-//							TODO:
-//							funzione che parsa input ed esegue un task diverso in base all'operazione ricevuta
 							buffer.clear();
 						}
-						
+
 						// Allow write operation on channel
 						client.register(selector, SelectionKey.OP_WRITE);
 
@@ -101,16 +101,16 @@ public class WQServer {
 						// Write the echo to the client
 						System.out.println("Channel is Writeable");
 						SocketChannel client = (SocketChannel) key.channel();
-						
+
 						String output = "oook!";
 						byte[] message = new String(output).getBytes();
 						ByteBuffer outBuffer = ByteBuffer.wrap(message);
-						
+
 						while (outBuffer.hasRemaining()) {
 							client.write(outBuffer);
 						}
 						outBuffer.clear();
-						
+
 						client.register(selector, SelectionKey.OP_READ);
 					}
 				} catch (IOException ex) {
@@ -123,23 +123,21 @@ public class WQServer {
 			}
 		}
 	}
-	
-	private static void handleClient(Message message, SocketChannel client, Selector selector, ThreadPoolExecutor tPool) {
-		
+
+	private static void handleClient(Message message, SocketChannel client, Selector selector,
+			ThreadPoolExecutor tPool) {
+
 		switch (message.operation) {
-		case "login":
-		case "add_friend":
-		case "logout":
-		case "friend_list":
-		case "score":
-		case "ranking":
-		case "challenge":
-		case "ans_challenge":
-		default:
-			break;
+			case "login":
+			case "add_friend":
+			case "logout":
+			case "friend_list":
+			case "score":
+			case "ranking":
+			case "challenge":
+			case "ans_challenge":
+			default:
+				break;
 		}
 	}
 }
-
-
-

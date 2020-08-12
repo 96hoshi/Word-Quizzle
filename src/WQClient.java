786b50ae49cd861861b189ac962ab0754f310ffe
96hoshi <m.locascio96@hotmail.com>
2020-08-12 18:31:21 +0200

    Fixed indentation

--- src/WQClient.java
+++ src/WQClient.java
@@ -10,9 +10,9 @@ import java.net.*;
 import java.io.IOException;
 
 public class WQClient {
-	
+
 	public static int PORT = 9999;
-	final static String serverHost = "localhost"; 
+	final static String serverHost = "localhost";
 
 	public static void main(String[] args) throws InterruptedException {
 		Scanner scan = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class WQClient {
 			SocketChannel client = SocketChannel.open(address);
 			ByteBuffer serverBuffer = ByteBuffer.allocate(516);
 
-			while(true) {
+			while (true) {
 				String input = scan.nextLine();
 
 //				Analyze and send the message to the server
@@ -35,20 +35,21 @@ public class WQClient {
 				System.out.println("Server: " + response);
 				serverBuffer.clear();
 			}
-			
+
 		} catch (IOException ex) {
 			scan.close();
 			ex.printStackTrace();
 		}
 	}
-	
+
 	private static boolean parseInput(String input, SocketChannel client) throws NullPointerException {
-		if (input == null) throw new NullPointerException();
+		if (input == null)
+			throw new NullPointerException();
 		System.out.println("Client " + client.toString() + ": " + input);
-		
+
 		MessageWorker msgWorker = new MessageWorker();
 		Message message = msgWorker.writeMessage(input);
-		
+
 //		Checks syntax and call the correct handler
 		switch (message.operation) {
 			case "register":
@@ -62,12 +63,14 @@ public class WQClient {
 				if (message.nickUser == null || message.opt == null) {
 					System.out.println("Select a valid username and password");
 					return false;
-				} break;
+				}
+				break;
 			case "add_friend":
 				if (message.nickUser == null || message.opt == null) {
 					System.out.println("Select a valid username and friendname");
 					return false;
-				} break;
+				}
+				break;
 			case "logout":
 			case "friend_list":
 			case "score":
@@ -75,7 +78,8 @@ public class WQClient {
 				if (message.nickUser == null) {
 					System.out.println("Select a valid username");
 					return false;
-				} break;
+				}
+				break;
 			case "challenge":
 				if (message.nickUser == null || message.opt == null) {
 					System.out.println("Select a valid username and friendname");
@@ -94,21 +98,21 @@ public class WQClient {
 		msgWorker.sendMessage(message, client);
 		return true;
 	}
-	
+
 	private static void handleRegister(String nickUser, String password) {
 
 		Registry registry = null;
 		RegistrationRemote remote = null;
 		try {
-			registry = LocateRegistry.getRegistry(serverHost, PORT+1);
+			registry = LocateRegistry.getRegistry(serverHost, PORT + 1);
 			remote = (RegistrationRemote) registry.lookup("WQ-Registration");
 			String response = remote.registerUser(nickUser, password);
 			System.out.println(response);
 		} catch (Exception e) {
-            e.printStackTrace();
+			e.printStackTrace();
 		}
 	}
-	
+
 	public static void handleChallenge(Message message, SocketChannel client) {
 //		aspetta per la risposta del server per dirti che l'amico ha accettato la sfida
 	}

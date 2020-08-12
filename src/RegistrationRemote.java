43045f3cdeb50b0f3816daa6befe915607c1c1bf
96hoshi <m.locascio96@hotmail.com>
2020-08-12 18:29:08 +0200

    Fixed indentation

--- src/RegistrationRemote.java
+++ src/RegistrationRemote.java
@@ -2,7 +2,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RegistrationRemote extends Remote {
-		
+
 	public String registerUser(String nickUser, String password) throws RemoteException;
 
 }

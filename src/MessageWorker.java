0cc1465f1553e2e141b5ea8f62122d7553c8ed7a
96hoshi <m.locascio96@hotmail.com>
2020-08-15 20:13:35 +0200

    Added utility functions for client and server comunication

--- src/MessageWorker.java
+++ src/MessageWorker.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
 import java.nio.charset.StandardCharsets;
 
@@ -7,25 +10,20 @@ import com.google.gson.Gson;
 
 public class MessageWorker {
 
-	Gson gson;
-	ByteBuffer outBuffer;
+	private Gson gson;
+	private ByteBuffer buffer;
 
 	public MessageWorker() {
 		gson = new Gson();
-	}
-
-	public Message readMessage(ByteBuffer buffer) {
-		String string = StandardCharsets.UTF_8.decode(buffer).toString();
-		Message msg = gson.fromJson(string, Message.class);
-
-		return msg;
+		buffer = ByteBuffer.allocate(516);
 	}
 
 	public Message writeMessage(String input) {
 		String s[] = input.split(" ");
 		Message msg = new Message();
 
-		msg.operation = s[0];
+		if (s.length > 0)
+			msg.operation = s[0];
 		if (s.length > 1)
 			msg.nickUser = s[1];
 		if (s.length == 3)
@@ -37,7 +35,7 @@ public class MessageWorker {
 	public void sendMessage(Message msg, SocketChannel sock) {
 		String output = gson.toJson(msg);
 		byte[] message = new String(output).getBytes();
-		outBuffer = ByteBuffer.wrap(message);
+		ByteBuffer outBuffer = ByteBuffer.wrap(message);
 
 		while (outBuffer.hasRemaining()) {
 			try {
@@ -47,6 +45,47 @@ public class MessageWorker {
 			}
 		}
 		outBuffer.clear();
+	}
+	
+	public String sendAndReceive(Message message, SocketChannel socket) {
+		sendMessage(message, socket);
+
+		try {
+			socket.read(buffer);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String response = new String(buffer.array(), StandardCharsets.US_ASCII);
+		buffer.clear();
+
+		return response;
+	}
+	
+	public Message readMessage(ByteBuffer buffer) {
+		String string = StandardCharsets.UTF_8.decode(buffer).toString();
+		Message msg = gson.fromJson(string, Message.class);
+
+		return msg;
+	}
+	
+	public void sendResponse(String response, SocketChannel client, Selector selector) {
+		byte[] message = new String(response).getBytes();
+		ByteBuffer outBuffer = ByteBuffer.wrap(message);
+
+		while (outBuffer.hasRemaining()) {
+			try {
+				client.write(outBuffer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		outBuffer.clear();
+
+		try {
+			client.register(selector, SelectionKey.OP_READ);
+		} catch (ClosedChannelException e) {
+			e.printStackTrace();
+		}
 	}
 }
 
